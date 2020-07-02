6095376995dc5833746e97b80c8e6e134e7eb3be
Jorge Saud <jsaud@modyo.com>
2020-07-02 14:07:04 -0400

    ci: remove error

--- src/wait.ts
+++ src/wait.ts
@@ -1,8 +1,8 @@
 export async function wait(milliseconds: number): Promise<string> {
   return new Promise(resolve => {
-    // if (isNaN(milliseconds)) {
-    throw new TypeError('milliseconds not a number')
-    // }
+    if (isNaN(milliseconds)) {
+      throw new TypeError('milliseconds not a number')
+    }
 
     setTimeout(() => resolve('done!'), milliseconds)
   })
