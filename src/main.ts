bc6fa843e01e5f4f732780fa4839df2df9fbf6d1
Jorge Saud <jsaud@modyo.com>
2020-07-02 15:42:47 -0400

    ci: test

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
 import {wait} from './wait'
+import * as artifact from '@actions/artifact'
 
 async function run(): Promise<void> {
   try {
@@ -15,6 +16,24 @@ async function run(): Promise<void> {
     core.debug(result)
     core.info('Output to the actions build log')
     core.setOutput('time', new Date().toTimeString())
+
+    const artifactClient = artifact.create()
+    const artifactName = 'my-artifact'
+    const files = ['README.md']
+    const rootDirectory = '.' // Also possible to use __dirname
+    const options = {
+      continueOnError: false,
+    }
+
+    const uploadResponse = await artifactClient.uploadArtifact(
+      artifactName,
+      files,
+      rootDirectory,
+      options,
+    )
+    core.setOutput('uploadResponse', uploadResponse)
+    core.info(JSON.stringify(uploadResponse))
+    core.debug(JSON.stringify(uploadResponse))
   } catch (error) {
     core.setFailed(error.message)
   }
